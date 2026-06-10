c967e0891ef9680bd9226399acb30e14c346043f
Usuario invitado <Guest@iMac-A10819.PUCE.EDU.EC>
2026-06-10 15:35:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mi-primer-trabajo',
+  webDir: 'dist'
+};
+
+export default config;
