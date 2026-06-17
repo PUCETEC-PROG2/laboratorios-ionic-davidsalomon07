b01c05f9bfb698ae24ddc52d517c8db49576bb38
Usuario invitado <Guest@iMac-A10819.PUCE.EDU.EC>
2026-06-17 15:42:53 -0500

    Se agrego css en tab3

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,37 +1,45 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab3.css';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab3.css";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Perfil de usuario</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Perfil de usuario</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
 
-
-        <div className='card-container'>
-          <IonCard>
-            <img 
-            src='https://avatars.githubusercontent.com/u/216223767?s=400&u=080bd4126623560cb471b5547a05d84d5e766f4b&v=4'
-            alt='Avatar'
+        <div className="card-container">
+          <IonCard className="card">
+            <img
+              src="https://avatars.githubusercontent.com/u/216223767?s=96&v=4"
+              alt="Avatar David"
             />
+
+            {/* IonCardHeader - Sirve para poner como si fuera un card */}
             <IonCardHeader>
-              <IonCardTitle>Luis David Salomon</IonCardTitle>
+              <IonCardTitle>David Salomon</IonCardTitle>
               <IonCardSubtitle>luisdavidsalomon</IonCardSubtitle>
             </IonCardHeader>
-            <IonCardContent>
-              Desarrollador de software con experiencia
-              en aplicaciones moviles y web.
-              Me gusta la tecnologia, aprender de manera continua y el deporte.
-            </IonCardContent>
+            <IonCardContent>Desarrollador de Software / Apasionado de los videojuegos y anime</IonCardContent>
           </IonCard>
         </div>
       </IonContent>
